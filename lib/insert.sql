db9ed6cd79f87e5fc2089252e05ae8dd833f2019
Zachary Farrell <zfarrell0507@gmail.com>
2022-09-12 15:04:03 -0400

    commit tree a346c9d35b8ed094dd7fb9321798d5c6f451a6e6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 6, "M", "Black", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 6, "M", "Black", "Mean", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 6, "F", "Brown", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 6, "M", "Brown", "Mean", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melissa", 6, "F", "Black", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 6, "M", "White", "Mean", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 6, "F", "Black", "Nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 8, "M", "Brown", "Nice", true);
